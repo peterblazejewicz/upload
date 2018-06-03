3128cd896a63f120fa8c1ae1da8d64a629783f91
Peter Blazejewicz <peter.blazejewicz@gmail.com>
2018-06-03 18:20:43 +0200

    Demo data change

--- js/demo/demo.controller.js
+++ js/demo/demo.controller.js
@@ -10,7 +10,12 @@
         status: 'Uploading',
       },
       { name: 'Hamlet.pdf', progress: 100, complete: false, held: false },
-      { name: 'Romeo and Juliet.pdf', progress: 100, complete: true, held: false },
+      {
+        name: 'Romeo and Juliet.pdf',
+        progress: 100,
+        complete: true,
+        held: false,
+      },
     ];
   }
   DemoController.nameToken = 'DemoController';
