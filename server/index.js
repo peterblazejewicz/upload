d83e04d1ca455fb20a142ea68e46c0b65b8f2f7b
Peter Blazejewicz <peter.blazejewicz@gmail.com>
2018-06-03 16:41:24 +0200

    Simple backent for upload

---
+++ server/index.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const fileUpload = require('express-fileupload');
+const app = express();
+
+// default options
+app.use(fileUpload());
+
+app.post('/upload', function (req, res) {
+  if (!req.files)
+    return res.status(400).send('No files were uploaded.');
+
+  // The name of the input field (i.e. "sampleFile") is used to retrieve the uploaded file
+  // let sampleFile = req.files.sampleFile;
+  console.log(req.files);
+  res.send('File uploaded!');
+  // Use the mv() method to place the file somewhere on your server
+  // sampleFile.mv('/somewhere/on/your/server/filename.jpg', function (err) {
+  //   if (err)
+  //     return res.status(500).send(err);
+
+  //   res.send('File uploaded!');
+  // });
+});
+app.listen(3001, () => console.log('Example app listening on port 3001!'))
