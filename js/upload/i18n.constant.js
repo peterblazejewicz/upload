92b3c4447694657a0d1c8de82e35cbe999c4f9aa
Peter Blazejewicz <peter.blazejewicz@gmail.com>
2018-06-03 17:57:48 +0200

    Derived work license

--- js/upload/i18n.constant.js
+++ js/upload/i18n.constant.js
@@ -1,3 +1,8 @@
+/**
+ * This work contains derivative code of Vaadin's file upload
+ * licensed under Apache License 2.0
+ * https://github.com/vaadin/vaadin-upload/blob/master/LICENSE
+ */
 (function(angular, undefined) {
   angular.module('sUploadModule').constant('sUploadModule.i18n', {
     dropFiles: {
