f21ece3486ffee2575217a053f164ced974eb2df
Peter Blazejewicz <peter.blazejewicz@gmail.com>
2018-06-03 19:04:56 +0200

    Port of upload

--- js/upload/s-upload.component.js
+++ js/upload/s-upload.component.js
@@ -150,15 +150,80 @@
     },
 
     _abortFileUpload: function(file) {
-      if(this.onUploadAbort) {
+      if (this.onUploadAbort) {
         this.onUploadAbort({
-          detail: {file, xhr: file.xhr},
+          detail: { file, xhr: file.xhr },
         });
       }
       file.abort = true;
       if (file.xhr) {
         file.xhr.abort();
       }
+    },
+
+    _configureXhr: function(xhr) {
+      if (typeof this.headers == 'string') {
+        try {
+          this.headers = JSON.parse(this.headers);
+        } catch (e) {
+          this.headers = undefined;
+        }
+      }
+      for (var key in this.headers) {
+        xhr.setRequestHeader(key, this.headers[key]);
+      }
+      if (this.timeout) {
+        xhr.timeout = this.timeout;
+      }
+      xhr.withCredentials = this.withCredentials;
+    },
+
+    _createXhr: function() {
+      return new XMLHttpRequest();
+    },
+
+    _formatFileProgress: function(file) {
+      return (
+        file.totalStr +
+        ': ' +
+        file.progress +
+        '% (' +
+        (file.loaded > 0
+          ? this.i18n.uploading.remainingTime.prefix + file.remainingStr
+          : this.i18n.uploading.remainingTime.unknown) +
+        ')'
+      );
+    },
+
+    _formatSize: function(bytes) {
+      if (typeof this.i18n.formatSize === 'function') {
+        return this.i18n.formatSize(bytes);
+      }
+
+      // https://wiki.ubuntu.com/UnitsPolicy
+      const base = this.i18n.units.sizeBase || 1000;
+      const unit = ~~(Math.log(bytes) / Math.log(base));
+      const dec = Math.max(0, Math.min(3, unit - 1));
+      const size = parseFloat((bytes / Math.pow(base, unit)).toFixed(dec));
+      return size + ' ' + this.i18n.units.size[unit];
+    },
+
+    _formatTime: function(seconds, split) {
+      if (typeof this.i18n.formatTime === 'function') {
+        return this.i18n.formatTime(seconds, split);
+      }
+
+      // Fill HH:MM:SS with leading zeros
+      while (split.length < 3) {
+        split.push(0);
+      }
+
+      return split
+        .reverse()
+        .map(number => {
+          return (number < 10 ? '0' : '') + number;
+        })
+        .join(':');
     },
 
     _isMultiple: function(maxFiles) {
@@ -182,10 +247,153 @@
       this.files.splice(this.files.indexOf(file), 1);
     },
 
+    _setStatus: function(file, total, loaded, elapsed) {
+      file.elapsed = elapsed;
+      file.elapsedStr = this._formatTime(
+        file.elapsed,
+        this._splitTimeByUnits(file.elapsed),
+      );
+      file.remaining = Math.ceil(elapsed * (total / loaded - 1));
+      file.remainingStr = this._formatTime(
+        file.remaining,
+        this._splitTimeByUnits(file.remaining),
+      );
+      file.speed = ~~(total / elapsed / 1024);
+      file.totalStr = this._formatSize(total);
+      file.loadedStr = this._formatSize(loaded);
+      file.status = this._formatFileProgress(file);
+    },
+
+    _splitTimeByUnits: function(time) {
+      const unitSizes = [60, 60, 24, Infinity];
+      const timeValues = [0];
+
+      for (var i = 0; i < unitSizes.length && time > 0; i++) {
+        timeValues[i] = time % unitSizes[i];
+        time = Math.floor(time / unitSizes[i]);
+      }
+
+      return timeValues;
+    },
+
     _uploadFile: function(file) {
       if (file.uploading) {
         return;
       }
+      var ini = Date.now();
+      var xhr = (file.xhr = this._createXhr(file));
+      var stalledId, last;
+      // onprogress is called always after onreadystatechange
+      xhr.upload.onprogress = e => {
+        clearTimeout(stalledId);
+        last = Date.now();
+        var elapsed = (last - ini) / 1000;
+        var loaded = e.loaded,
+          total = e.total,
+          progress = ~~((loaded / total) * 100);
+        file.loaded = loaded;
+        file.progress = progress;
+        file.indeterminate = loaded <= 0 || loaded >= total;
+        if (file.error) {
+          file.indeterminate = file.status = undefined;
+        } else if (!file.abort) {
+          if (progress < 100) {
+            this._setStatus(file, total, loaded, elapsed);
+            stalledId = setTimeout(() => {
+              file.status = this.i18n.uploading.status.stalled;
+            }, 2000);
+          } else {
+            file.loadedStr = file.totalStr;
+            file.status = this.i18n.uploading.status.processing;
+            file.uploading = false;
+          }
+        }
+        if (this.onUploadProgress) {
+          this.onUploadProgress({
+            detail: { file, xhr },
+          });
+        }
+      };
+
+      // More reliable than xhr.onload
+      xhr.onreadystatechange = () => {
+        if (xhr.readyState == 4) {
+          clearTimeout(stalledId);
+          file.indeterminate = file.uploading = false;
+          if (file.abort) {
+            return;
+          }
+          file.status = '';
+          // Custom listener can modify the default behavior either
+          // preventing default, changing the xhr, or setting the file error
+          if (this.onUploadReponse) {
+            this.onUploadReponse({
+              detail: { file, xhr },
+            });
+          }
+          // TODO implementation
+          // if (!evt) {
+          //   return;
+          // }
+          if (xhr.status === 0) {
+            file.error = this.i18n.uploading.error.serverUnavailable;
+          } else if (xhr.status >= 500) {
+            file.error = this.i18n.uploading.error.unexpectedServerError;
+          } else if (xhr.status >= 400) {
+            file.error = this.i18n.uploading.error.forbidden;
+          }
+          file.complete = !file.error;
+          if (file.error) {
+            this.onUploadError &&
+              this.onUploadError({
+                detail: { file, xhr },
+              });
+          } else {
+            this.onUploadSuccess({
+              detail: { file, xhr },
+            });
+          }
+        }
+      };
+
+      var formData = new FormData();
+
+      file.uploadTarget = this.target || '';
+      file.formDataName = this.formDataName;
+      this.onUploadBefore &&
+        this.onUploadBefore({
+          detail: { file, xhr },
+        });
+      // TODO cancellation
+      // if (!evt) {
+      //   return;
+      // }
+      formData.append(file.formDataName, file, file.name);
+      xhr.open(this.method, file.uploadTarget, true);
+      this._configureXhr(xhr);
+
+      file.status = this.i18n.uploading.status.connecting;
+      file.uploading = file.indeterminate = true;
+      file.complete = file.abort = file.error = file.held = false;
+
+      xhr.upload.onloadstart = () => {
+        this.onUploadStart &&
+          this.onUploadStart({
+            detail: { file, xhr },
+          });
+      };
+
+      // Custom listener could modify the xhr just before sending it
+      // preventing default
+      this.onUploadRequest &&
+        this.onUploadRequest({
+          detail: { file, xhr, formData },
+        });
+      // TODO cancellation
+      // if (uploadEvt) {
+      //   xhr.send(formData);
+      // }
+      xhr.send(formData);
     },
   };
   UploadController.$inject = [
@@ -203,6 +411,13 @@
       maxFileSize: '<',
       method: '<',
       onFileReject: '&',
+      onUploadBefore: '&',
+      onUploadStart: '&',
+      onUploadSuccess: '&',
+      onUploadError: '&',
+      onUploadRequest: '&',
+      onUploadReponse: '&',
+      onUploadProgress: '&',
       target: '<',
       timeout: '<',
     },
