b98e7bcb7a5480f9d7cf219da4201dcc6d11c48f
Peter Blazejewicz <peter.blazejewicz@gmail.com>
2018-06-04 23:24:13 +0200

    No default files on start

--- js/demo/demo.controller.js
+++ js/demo/demo.controller.js
@@ -1,22 +1,6 @@
 (function(angular, undefined) {
   function DemoController() {
-    this.files = [
-      {
-        name: 'Don Quixote.pdf',
-        progress: 50,
-        complete: false,
-        uploading: true,
-        held: false,
-        status: 'Uploading',
-      },
-      { name: 'Hamlet.pdf', progress: 100, complete: false, held: false },
-      {
-        name: 'Romeo and Juliet.pdf',
-        progress: 100,
-        complete: true,
-        held: false,
-      },
-    ];
+    this.files = [];
   }
   DemoController.prototype = {
     onFileReject: function(detail) {
